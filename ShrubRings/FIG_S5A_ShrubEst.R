324500848730aef44568670eb6f1a7a1f72018fb
TrantsPlants <66100531+TrantsPlants@users.noreply.github.com>
2020-06-05 14:18:29 -0400

    Made some more changes

--- ShrubRings/FIG_S5A_ShrubEst.R
+++ ShrubRings/FIG_S5A_ShrubEst.R
@@ -12,7 +12,7 @@ library(dplyr)
 # Load up data
 rm(list=ls())
 
-ind.samp <- read.csv("AllIndSamples.csv") %>%
+ind.samp <- read.csv("~/NakvakBrook/ShrubRings/AllIndSamples.csv") %>%
   mutate()
 
 #Establishment plot - Histogram with climate overlay
@@ -38,18 +38,18 @@ mean(ave.clim$Tave_07)
 ggplot()+
   geom_histogram(data = ind.samp, aes(x = MeanEst, fill = SPP), binwidth = 1, color = 'black', size = 0.2)+
   #ggtitle("Shrub establishment dates by species")+
-  scale_fill_npg(alpha = 0.7)+
+  scale_fill_jco(alpha = 0.6)+
   xlab('Establishment year')+
   ylab('No. ramets established')+
   labs(fill = "Species")+
   geom_line(data = filter(clim, Year > 1996), aes(x = Year, y = (Tave_07-5)))+
   xlim(1996, 2015)+
-  scale_y_continuous(sec.axis = sec_axis(~.+5, name = "August temperature (°C)"))+
+  scale_y_continuous(sec.axis = sec_axis(~.+5, name = "Mean July temperature (°C)"))+
   theme_pubr(base_size = 10, legend = 'bottom')
 
-#5 x 5 is good
+#Save dimensions: 5" x 5"
 
-#Creating AllSamples.csv
+#Creating 'AllSamples.csv' file ---
 nb1<-read.rwl("AllSamplesNB1.csv")
 nb2<-read.rwl("AllSamplesNB2.csv")
 nb3<-read.rwl("AllSamplesNB3.csv")
